f3cfdc5d98d5713ebb07b2652d312975d664160f
Joshua D Harvey <65784851+JDanielHarvey@users.noreply.github.com>
2021-08-25 11:40:54 -0700

    Minor Mod to comments

--- Scripts - SQL/sql_functions/udfAVP_ParseSingleUTM.sql
+++ Scripts - SQL/sql_functions/udfAVP_ParseSingleUTM.sql
@@ -12,16 +12,12 @@ create or alter function dbo.ParseSingleUTM(@utm nvarchar(20), @utmString nvarch
 		utm_campaign=
 		utm_content=
 		utm_keyword=
+      e.g.
+        utm_source=yelp&utm_medium=local&utm_campaign=clinic-suncity&utm_content=appt
 */
 
 begin 
 
---	--> used for development 
---declare @utmString nvarchar(255)
---set @utmString = 'utm_source=yelp&utm_medium=local&utm_campaign=clinic-suncity&utm_content=appt'
-
---declare @utm nvarchar(20)
---set @utm = 'utm_campaign='
 
 --> ensures an equal sign has been included on the @utm parameter for the function
 if charindex('=', 'utm_campaign') = 0
